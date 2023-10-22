325119622e15e567fd03f5ea6e0a9281e82bbdb0
Konstantin <judge.konstantin@gmail.com>
2023-10-22 19:21:40 +0300

    Commit 2 Сделал, чтобы брались коэффициенты мосбиржи и инфляции из отдельного класса

--- src/Main.java
+++ src/Main.java
@@ -2,53 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double[] moex = {
-                417.42,
-                673.72,
-                722.81,
-                1323.32,
-                2216.63,
-                2472.38,
-                810.922,
-                1793.24,
-                2209.46,
-                1835.14,
-                1934.43,
-                1967.83,
-                1828.06,
-                2305.50,
-                3042.00,
-                3015.71,
-                3564.05,
-                4887.25,
-                5567.28,
-                6731.43,
-                4170.35
-        };
 
-        double[] inflation = {
-                15.06,
-                11.99,
-                11.74,
-                10.91,
-                9.00,
-                11.87,
-                13.28,
-                8.80,
-                8.78,
-                6.10,
-                6.58,
-                6.45,
-                11.36,
-                12.91,
-                5.38,
-                2.52,
-                4.27,
-                03.05,
-                4.91,
-                8.39,
-                11.92,
-        };
+        double[] moex = Constants.MOEX_RATE;
+        double[] inflation = Constants.INFLATION_RATE;
+
 
         // Нам нужно получить ввод от пользователя
         Scanner sc = new Scanner(System.in);
